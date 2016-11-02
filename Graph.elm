8f05b5215693fa2b88562dbc504e4e0444345a1a
Tessa Kelly <tessakkelly@gmail.com>
2016-11-01 21:51:52 -0700

    Begin mocking out general approach

--- Graph.elm
+++ Graph.elm
@@ -1,16 +1,63 @@
-module Graph exposing (Model, (=>))
+module Graph exposing (model, (=>))
 
 {-| Graph experimentally provides an interface for declaring graph-relationships and displaying.
 -}
 
+import Dict
+import Html
+import Set
+
+
 --MODEL
 
 
-{-| List out each relationship you'd like to describe.
--}
-type alias Model a =
-    { relations : List ( a, a )
+type alias Model comparable =
+    { relations : Set.Set comparable
     }
+
+
+{-| List out each relationship you'd like to describe, and use `model` to initialize.
+-}
+model : List ( comparable, comparable ) -> Model comparable
+model relations =
+    { relations = process relations }
+
+
+process : List ( comparable, comparable ) -> Set.Set comparable
+process relations =
+    findDisconnectedCentralNodes relations
+        |> (\_ -> Set.empty)
+
+
+findDisconnectedCentralNodes : List ( comparable, comparable ) -> Dict.Dict comparable Int
+findDisconnectedCentralNodes relations =
+    List.foldl countOccurrences Dict.empty relations
+
+
+countOccurrences : ( comparable, comparable ) -> Dict.Dict comparable Int -> Dict.Dict comparable Int
+countOccurrences items dict =
+    dict
+        |> countOccurrence (fst items)
+        |> countOccurrence (snd items)
+
+
+countOccurrence : comparable -> Dict.Dict comparable Int -> Dict.Dict comparable Int
+countOccurrence item dict =
+    Dict.get item dict
+        |> Maybe.withDefault 0
+        |> \occurrence ->
+            Dict.insert item (occurrence + 1) dict
+
+
+
+--VIEW
+
+
+{-| `view` produces a visualization based on the model passed in.
+-}
+view : Model comparable -> Html.Html msg
+view model =
+    Html.div [] []
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # elm-graph-exploration
+
+# General Approach
+
+1. Isolate distinct graphs
+2. For each distinct graph,
+  a. Find the point/s with the most edges
+  b. Put these points in the center
+  c. Work out radially from there, allocating spacing according to number of additional edges
+
+# TODO
+- worry about edges cutting across each other
